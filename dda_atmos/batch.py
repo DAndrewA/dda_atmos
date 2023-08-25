590abf1a8ad97ceabd071d86670a044f2467cc69
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-08-25 09:49:49 +0100

    batch: started adding batch objects for kernal creation and density calcuoation. Shelving this branch for later

--- dda_atmos/batch.py
+++ dda_atmos/batch.py
@@ -5,6 +5,7 @@ Creation Date: 24/8/23
 from dda_atmos.dda_data import DDA_data
 from . import steps
 from dda_data import DDA_data
+import copy
 
 class Batch:
     '''Class representing a batch of steps for executing in the DDA algorithm. A batch object can contain other batch objects to execute.'''
@@ -13,8 +14,37 @@ class Batch:
         '''Initialisation function.'''
     
     def execute_batch(self, input_data: DDA_data):
-        raise NotImplementedError('Use an instance of Batch that isnt the base Batch class.')
+        return copy.copy(input_data) # return a shallow copy of input_data, to allow a new instance that references data in the original, saving on memory...
     
+
+class CreateKernal(Batch):
+    def __init__(self,**kernal_args):
+        '''Initialisation function. For kernal creation, requires the kernal parameters to be passed to it.
+        
+        INPUTS:
+
+
+        '''
+        super().__init__()
+        self.kernal = steps.create_kernal.Gaussian(**kernal_args)
+
+    def execute_batch(self, input_data: DDA_data):
+        '''Sets the 'kernal' parameter on the new_data to the kernal.'''
+        new_data = super().execute_batch(input_data)
+        new_data.kernal = self.kernal
+        return new_data
+    
+
+class CalculateDensity(Batch):
+    def __init__(self, **density_parameters):
+        super().__init__()
+        '''Initialisation function. For density calculation, requires density calculation parameters to be passed.'''
+        self.density_parameters = density_parameters
+
+    def execute_batch(self, input_data: DDA_data, data_key: str = 'data_in'):
+        new_data = super().execute_batch(input_data)
+        '''Calculates the density field of the input_data[data_key] data.'''
+
 
 class SinglePass(Batch):
     '''Batch of algorithm steps to create a density field, calculate the threshold and determine the cloud mask from that.'''
@@ -29,4 +59,5 @@ class SinglePass(Batch):
         self.pass_num = pass_num
 
     def execute_batch(self, input_data: DDA_data):
-        return super().execute_batch(input_data)
+        new_data = super().execute_batch(input_data)
+        '''SinglePass will run the batches for creating the kernal, density field, thresholds and cloudmask.'''
